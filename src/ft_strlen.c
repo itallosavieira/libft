ae83f7133773fd7301f46e7031f932f0d97ae017
itallosavieira <itallo.sa.vieira@gmail.com>
2021-05-20 23:06:39 -0300

    Add ft_strlen

---
+++ src/ft_strlen.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+unsigned int	ft_strlen(char *s) {
+	int i;
+	i = 0;
+	
+	while (s[i])
+		i++;
+	
+	return (i);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include "libft.h"
+#include <stdio.h>
+
+int	main(void) {
+	printf("\n#  strlen: 5  #\n");
+	int n = ft_strlen("tallo");
+	printf("#  > test: %d  #\n", n);
+	return (0);
+}
