f2f6e08a7e43134f24dd1b8f665d459290ee2185
itallosavieira <itallo.sa.vieira@gmail.com>
2021-06-08 03:41:54 -0300

    add ft_strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -1,28 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: isa-viei <isa-viei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/06/02 22:17:22 by isa-viei          #+#    #+#             */
-/*   Updated: 2021/06/08 03:36:50 by isa-viei         ###   ########.fr       */
+/*   Created: 2021/06/08 03:37:19 by isa-viei          #+#    #+#             */
+/*   Updated: 2021/06/08 03:41:22 by isa-viei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
-
-int main()
+char	*ft_strchr(const char *s, int c)
 {
-	const char str[] = "http://www.tutorialspoint.com";
-	const char ch = '.';
-	char *ret;
-
-	ret = ft_strchr(str, ch);
-
-	printf("String after |%c| is - |%s|\n", ch, ret);
-
-	return(0);
+	while (*s++)
+	{
+		if (*s == c)
+			return ((char *)s);
+	}
+	return (NULL);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: isa-viei <isa-viei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/28 02:02:00 by isa-viei          #+#    #+#             */
-/*   Updated: 2021/06/08 03:29:09 by isa-viei         ###   ########.fr       */
+/*   Updated: 2021/06/08 03:41:11 by isa-viei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,7 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
+char	*ft_strchr(const char *s, int c);
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 size_t	ft_strlen(const char *s);
